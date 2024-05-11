99f126d298335ce857e1f1b383e31ad9c713eea0
Nick Taylor <nick@nickyt.co>
2024-05-10 22:36:43 -0400

    chore: initial commit

---
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import * as Clerk from '@clerk/elements/common';
+import * as SignIn from '@clerk/elements/sign-in';
+
+export default function SignInPage() {
+  return (
+    <div className="grid w-full flex-grow items-center bg-black px-4 sm:justify-center h-dvh">
+      <SignIn.Root>
+        <SignIn.Step
+          name="start"
+          className="w-full flex-grow space-y-6 rounded-2xl bg-neutral-900 bg-[radial-gradient(circle_at_50%_0%,theme(colors.white/10%),transparent)] px-4 py-10 ring-1 ring-inset ring-white/5 sm:w-96 sm:px-8"
+        >
+          <header className="text-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 40" className="mx-auto size-10">
+              <mask id="a" width="40" height="40" x="0" y="0" maskUnits="userSpaceOnUse">
+                <circle cx="20" cy="20" r="20" fill="#D9D9D9" />
+              </mask>
+              <g fill="#fff" mask="url(#a)">
+                <path d="M43.5 3a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46V2ZM43.5 8a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46V7ZM43.5 13a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 18a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 23a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 28a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 33a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1ZM43.5 38a.5.5 0 0 0 0-1v1Zm0-1h-46v1h46v-1Z" />
+                <path d="M27 3.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 8.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM23 13.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM21.5 18.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM20.5 23.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM22.5 28.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM25 33.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2ZM27 38.5a1 1 0 1 0 0-2v2Zm0-2h-46v2h46v-2Z" />
+              </g>
+            </svg>
+            <h1 className="mt-4 text-xl font-medium tracking-tight text-white">Sign in to Clover</h1>
+          </header>
+          <Clerk.GlobalError className="block text-sm text-red-400" />
+          <div className="space-y-2">
+            <Clerk.Connection
+              name="github"
+              className="flex w-full items-center justify-center gap-x-3 rounded-md bg-neutral-700 px-3.5 py-1.5 text-sm font-medium text-white shadow-[0_1px_0_0_theme(colors.white/5%)_inset,0_0_0_1px_theme(colors.white/2%)_inset] outline-none hover:bg-gradient-to-b hover:from-white/5 hover:to-white/5 focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-white active:bg-gradient-to-b active:from-black/20 active:to-black/20 active:text-white/70"
+            >
+              Login with GitHub
+            </Clerk.Connection>
+          </div>
+          <p className="text-center text-sm text-neutral-400">
+            No account?{' '}
+            <a
+              href="#"
+              className="font-medium text-white decoration-white/20 underline-offset-4 outline-none hover:underline focus-visible:underline"
+            >
+              Create an account
+            </a>
+          </p>
+        </SignIn.Step>
+      </SignIn.Root>
+    </div>
+  );
+}
